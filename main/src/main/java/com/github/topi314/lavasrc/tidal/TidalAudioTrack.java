02a722bb7cdcb5acbdee7a2dfb96f530194be721
appujet <sdipedit@gmail.com>
2025-03-21 00:25:06 +0530

    Refactor TidalAudioTrack to simplify constructor and handle unsupported preview operation

--- main/src/main/java/com/github/topi314/lavasrc/tidal/TidalAudioTrack.java
+++ main/src/main/java/com/github/topi314/lavasrc/tidal/TidalAudioTrack.java
@@ -1,7 +1,6 @@
 package com.github.topi314.lavasrc.tidal;
 
 import com.github.topi314.lavasrc.mirror.MirroringAudioTrack;
-import com.sedmelluq.discord.lavaplayer.container.mp3.Mp3AudioTrack;
 import com.sedmelluq.discord.lavaplayer.tools.io.SeekableInputStream;
 import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
 import com.sedmelluq.discord.lavaplayer.track.AudioTrackInfo;
@@ -10,16 +9,16 @@ import com.sedmelluq.discord.lavaplayer.track.InternalAudioTrack;
 public class TidalAudioTrack extends MirroringAudioTrack {
 
 	public TidalAudioTrack(AudioTrackInfo trackInfo, TidalSourceManager sourceManager) {
-		this(trackInfo, null, null, null, null, null, false, sourceManager);
+		this(trackInfo, null, null, null, null, sourceManager);
 	}
 
-	public TidalAudioTrack(AudioTrackInfo trackInfo, String albumName, String albumUrl, String artistUrl, String artistArtworkUrl, String previewUrl, boolean isPreview, TidalSourceManager sourceManager) {
-		super(trackInfo, albumName, albumUrl, artistUrl, artistArtworkUrl, previewUrl, isPreview, sourceManager);
+	public TidalAudioTrack(AudioTrackInfo trackInfo, String albumName, String albumUrl, String artistUrl, String artistArtworkUrl, TidalSourceManager sourceManager) {
+		super(trackInfo, albumName, albumUrl, artistUrl, artistArtworkUrl, null, false, sourceManager);
 	}
 
 	@Override
 	protected InternalAudioTrack createAudioTrack(AudioTrackInfo trackInfo, SeekableInputStream stream) {
-		return new Mp3AudioTrack(trackInfo, stream);
+		throw new UnsupportedOperationException("Previews are not supported by Tidal");
 	}
 
 	@Override
